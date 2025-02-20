d86a3147b19803a2df50bdb843352c15800686fa
mujtaba ahmad <mujtabaahmad4200@example.com>
2025-02-20 22:50:28 +0500

    90% complete

---
+++ js.js
@@ -0,0 +1,12 @@
+function showSideBar() {
+  const sidebar = document.querySelector('.sidebar'); // Corrected selector
+  sidebar.style.display = 'flex';
+}
+
+function closeSidebar() {
+  const sidebar = document.querySelector('.sidebar'); // Select the sidebar again
+  sidebar.style.display = 'none';
+}
+
+
+
